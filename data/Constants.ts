53ee3e2e325f039ee65a551b4fe12c51717d604c
Joseph Huntley <Joseph.Huntley@outlook.com>
2023-04-03 14:42:46 -0400

    Added Constants file

---
+++ data/Constants.ts
@@ -0,0 +1,30 @@
+export const Components = [
+  {
+    category: "Layout",
+    items: ["Box", "Center", "Container"],
+  },
+  {
+    category: "Forms",
+    items: ["Button", "Switch"],
+  },
+  {
+    category: "Data Display",
+    items: ["Badge", "Card", "Divider", "Kbd Key", "Text"],
+  },
+  {
+    category: "Feedback",
+    items: ["Progress", "Skeleton"],
+  },
+  {
+    category: "Overlay",
+    items: ["Drawer", "Menu", "Modal", "Popover", "ToolTip"],
+  },
+  {
+    category: "Disclosure",
+    items: ["Accordion", "Carousel", "Tabs"],
+  },
+  {
+    category: "Other",
+    items: ["Backdrop", "Close BUtton", "Media Query", "Portal"],
+  },
+]

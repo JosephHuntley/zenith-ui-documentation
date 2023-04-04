64b90c89e38c442b2af2d5027f91451ad2bf27a6
Joseph Huntley <Joseph.Huntley@outlook.com>
2023-04-04 12:03:07 -0400

    Header is now absolute

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,25 +1,41 @@
-import React from "react"
-import { Box, Text } from "zenith-ui"
+import React, { useContext } from "react"
+import { Box, Container, Text, useDarkMode } from "zenith-ui"
 import styles from "./Header.module.css"
 import { useRouter } from "next/router"
+import { AiFillGithub } from "react-icons/ai"
+import { CiSun } from "react-icons/ci"
+import { BsMoonFill } from "react-icons/bs"
+import { ThemeContext } from "../../styles/Theme"
 
 const Header = () => {
+  const { theme, themeToggler } = useContext(ThemeContext)
   const router = useRouter()
   return (
-    <header>
+    <header className={styles.header}>
       <Box
         className={`${styles.header} cursor`}
-        size={{ width: "100%", height: "15vh" }}
-        flex={{ direction: "row", gap: "1rem" }}
+        size={{ width: "100%", height: "100%" }}
+        flex={{ direction: "row", justify: "space-between" }}
         padding={{ p: "1.5rem" }}
       >
         <Text
           variant="p"
           font={{ size: "4rem" }}
           onClick={() => router.push("/")}
+          className="logo"
         >
           Zenith-UI
         </Text>
+        <Container flex={{ gap: "1rem" }}>
+          <AiFillGithub className={styles.icon} />
+          <Container onClick={themeToggler}>
+            {theme === "dark" ? (
+              <BsMoonFill className={styles.icon} />
+            ) : (
+              <CiSun className={styles.icon} />
+            )}
+          </Container>
+        </Container>
       </Box>
     </header>
   )
--- src/layout/Sidebar/Sidebar.tsx
+++ src/layout/Sidebar/Sidebar.tsx
@@ -7,10 +7,10 @@ const Sidebar = () => {
   const router = useRouter()
   return (
     <Box
-      size={{ width: "15vw", height: "calc(100%-10vh)" }}
+      size={{ width: "15vw", height: "calc(100%-7rem)" }}
       flex={{ direction: "column", gap: "2rem" }}
-      position={{ position: "absolute", top: "15vh" }}
-      padding={{ x: "1.5rem", bottom: "2rem" }}
+      position={{ position: "absolute", top: "7rem" }}
+      padding={{ x: "1.5rem", y: "2rem" }}
     >
       {Components.map((data) => (
         <Container

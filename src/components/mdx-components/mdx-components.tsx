e59a3682095e6a7065ccc922456b3b7db4506b1f
Joseph Huntley <Joseph.Huntley@outlook.com>
2023-04-06 11:22:34 -0400

    Added inline code element

--- src/components/mdx-components/mdx-components.tsx
+++ src/components/mdx-components/mdx-components.tsx
@@ -1,6 +1,8 @@
 import { Text, Box } from "zenith-ui"
 import * as components from "zenith-ui"
 import CodeBlock from "./code/CodeBlock"
+import BaseHighlight from "prism-react-renderer"
+import { theme } from "./code/customTheme"
 
 export const MDXComponents = {
   ...components,
@@ -10,6 +12,15 @@ export const MDXComponents = {
   h4: (props: any) => <Text variant="h4" {...props} />,
   h5: (props: any) => <Text variant="h5" {...props} />,
   h6: (props: any) => <Text variant="h6" {...props} />,
-  p: (props: any) => <Text {...props} />,
+  p: (props: any) => <Text margin={{ y: ".5rem" }} {...props} />,
   pre: (props: any) => <CodeBlock {...props} />,
+  code: (props: any) => (
+    <code
+      style={{ color: "rgba(92, 207, 230, 1)", margin: "0 .5rem" }}
+      {...props}
+    />
+  ),
+  li: (props: any) => (
+    <li style={{ display: "flex", marginBottom: ".1rem" }} {...props} />
+  ),
 }

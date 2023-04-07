55b40281b4fa4ba53159adf4574a4b0ca69a0675
Joseph Huntley <Joseph.Huntley@outlook.com>
2023-04-07 12:32:54 -0400

    Built Menu Component

--- src/layout/Menu/Menu.tsx
+++ src/layout/Menu/Menu.tsx
@@ -1,12 +1,57 @@
-import React, { useState } from "react"
-import { Drawer } from "zenith-ui"
+import React from "react"
+import { Container, Drawer, Portal, Text } from "zenith-ui"
+import { Components } from "data/Constants"
+import { useRouter } from "next/router"
 
-const Menu = () => {
-  const [isOpen, setIsOpen] = useState(true)
+export type MenuProps = {
+  isOpen: boolean
+  setIsOpen: Function
+}
+
+const Menu = ({ isOpen, setIsOpen }: MenuProps) => {
+  const router = useRouter()
+
   return (
-    <Drawer isOpen={isOpen} sm={{ margin: { m: "0" } }}>
-      This is a test
-    </Drawer>
+    <Portal>
+      <Drawer
+        isOpen={isOpen}
+        top={"8rem"}
+        md={{
+          size: { height: "calc(100vh - 8rem)", width: "75vw" },
+          padding: { x: "3rem" },
+        }}
+        sm={{ size: { height: "calc(100vh - 8rem)", width: "100vw" } }}
+      >
+        {Components.map((data) => (
+          <Container
+            flex={{ gap: ".5rem", direction: "column" }}
+            key={data.category}
+          >
+            <Text
+              variant="h2"
+              md={{ font: { size: "5.5rem" }, margin: { top: "1rem" } }}
+              sm={{ font: { size: "4rem" } }}
+            >
+              {data.category}
+            </Text>
+            {data.items.map((title) => (
+              <Container
+                className="cursor"
+                key={title}
+                onClick={() => router.push(`/docs/${title.toLowerCase()}`)}
+              >
+                <Text
+                  md={{ font: { size: "4rem" } }}
+                  sm={{ font: { size: "3rem" } }}
+                >
+                  {title}
+                </Text>
+              </Container>
+            ))}
+          </Container>
+        ))}
+      </Drawer>
+    </Portal>
   )
 }
 
